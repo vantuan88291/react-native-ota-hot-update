f4bef9ff9a67d6e6338b4e5521bf94adc616c643
vantuan88291 <vantuan88291@gmail.com>
2024-08-19 16:54:34 +0700

    Add demo manage version

--- App.tsx
+++ App.tsx
@@ -6,14 +6,11 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
   Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
-  Text,
   useColorScheme,
   View,
 } from 'react-native';
@@ -21,40 +18,10 @@ import {
 import {
   Colors,
   Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 import hotUpdate from 'RNhotupdate';
+import {useCheckVersion} from './useCheckVersion.ts';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -62,24 +29,9 @@ function App(): React.JSX.Element {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
-  const [loaded, setLoaded] = React.useState(false);
-  const [err, setErr] = React.useState('');
-
-  const getData = async () => {
-    const version = await hotUpdate.getCurrentVersion();
-    console.log('v----', version);
-    hotUpdate.downloadBundleUri('https://raw.githubusercontent.com/vantuan88291/react-native-ota-hot-update/main/android/output/index.android.bundle.zip', 1, {
-      updateSuccess: () => {
-        setLoaded(true);
-      },
-      updateFail(message?: string) {
-        setErr(message + '');
-      },
-      restartAfterInstall: true,
-    });
-  };
+  useCheckVersion();
   const deleteUpdate = () => {
-    hotUpdate.removeUpdate();
+    hotUpdate.removeUpdate(true);
   };
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -91,39 +43,14 @@ function App(): React.JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Button title={'Click on update'} onPress={getData} />
         <Button title={'Click on delete update'} onPress={deleteUpdate} />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title={loaded ? 'Updated' : 'not update'}>
-            <ReloadInstructions />
-          </Section>
-          <Section title={err} />
-        </View>
+          }} />
       </ScrollView>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
---
+++ useCheckVersion.ts
@@ -0,0 +1,44 @@
+import React from 'react';
+import hotUpdate from 'RNhotupdate';
+import {Alert, Platform} from 'react-native';
+
+const apiVersion = 'https://firebasestorage.googleapis.com/v0/b/ota-demo-68f38.appspot.com/o/update.json?alt=media&token=8bea750d-d57e-4d70-9334-f846f25f344b';
+export const useCheckVersion = () => {
+
+  const startUpdate = async (url: string, version: number) => {
+    hotUpdate.downloadBundleUri(url, version, {
+      updateSuccess: () => {
+        console.log('update success!');
+      },
+      updateFail(message?: string) {
+        Alert.alert('Update failed!', message, [
+          {
+            text: 'Cancel',
+            onPress: () => console.log('Cancel Pressed'),
+            style: 'cancel',
+          },
+        ]);
+      },
+      restartAfterInstall: true,
+    });
+  };
+  const onCheckVersion = () => {
+    fetch(apiVersion).then(async (data) => {
+      const result = await data.json();
+      const currentVersion = await hotUpdate.getCurrentVersion();
+      if (result?.version > currentVersion) {
+        Alert.alert('New version is comming!', 'New version has release, please update', [
+          {
+            text: 'Cancel',
+            onPress: () => console.log('Cancel Pressed'),
+            style: 'cancel',
+          },
+          {text: 'Update', onPress: () => startUpdate(Platform.OS === 'ios' ? result?.downloadIosUrl : result?.downloadAndroidUrl, result.version)},
+        ]);
+      }
+    });
+  };
+  React.useEffect(() => {
+    onCheckVersion();
+  }, []);
+};
